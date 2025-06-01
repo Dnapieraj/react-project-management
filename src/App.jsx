1de18aa23a06957f973ea04b37171efb87f909db
Daniel <d.napieraj06@gmail.com>
2025-06-01 13:17:08 +0200

    Implement project creation flow by integrating NewProject component and updating NoProjectSelected and ProjectsSidebar components to handle project addition.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import ProjectsSidebar from './components/ProjectsSidebar'
-// import NewProject from './components/NewProject'
+import NewProject from './components/NewProject'
 import NoProjectSelected from './components/NoProjectSelected'
+import { useState } from 'react'
 
 function App() {
+	const [projectsState, setProjectState] = useState({
+		selectedProjectId: undefined,
+		projects: [],
+	})
+	const handleStartAddProject = () => {
+		setProjectState(prevState => {
+			return {
+				...prevState,
+				selectedProjectId: null,
+			}
+		})
+	}
+	let content
+	if (projectsState.selectedProjectId === null) {
+		content = <NewProject />
+	} else if (projectsState.selectedProjectId === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+	}
 	return (
-		<main className="h-screen my-8 flex gap-8 ">
-			<ProjectsSidebar />
-			<NoProjectSelected />
+		<main className="h-screen  flex gap-8 ">
+			<ProjectsSidebar onStartAddProject={handleStartAddProject} />
+			{content}
 		</main>
 	)
 }
