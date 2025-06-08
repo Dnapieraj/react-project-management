3df51bcbc4c2ce9439a997750e0521d005a5568e
Daniel <d.napieraj06@gmail.com>
2025-06-08 22:12:28 +0200

    Add Modal component and integrate error handling in NewProject for form validation

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -10,6 +10,10 @@ export default function NewProject({ onAdd }) {
 		const enteredDescription = description.current.value
 		const enteredDueDate = dueDate.current.value
 
+		if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+			//show the error modal
+		}
+
 		onAdd({
 			title: enteredTitle,
 			description: enteredDescription,
@@ -18,22 +22,26 @@ export default function NewProject({ onAdd }) {
 	}
 
 	return (
-		<div className="w-[35rem]">
-			<menu className="flex items-center justify-end gap-4 my-4">
-				<li>
-					<button className="text-stone-800 hover:text-stone-950">Cancel</button>
-				</li>
-				<li>
-					<button className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>
-						Save
-					</button>
-				</li>
-			</menu>
-			<div>
-				<Input type="text" ref={title} label="Title" />
-				<Input ref={description} label="Description" textarea />
-				<Input type="date" ref={dueDate} label="Due Date" />
+		<>
+			<div className="w-[35rem]">
+				<menu className="flex items-center justify-end gap-4 my-4">
+					<li>
+						<button className="text-stone-800 hover:text-stone-950">Cancel</button>
+					</li>
+					<li>
+						<button
+							className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+							onClick={handleSave}>
+							Save
+						</button>
+					</li>
+				</menu>
+				<div>
+					<Input type="text" ref={title} label="Title" />
+					<Input ref={description} label="Description" textarea />
+					<Input type="date" ref={dueDate} label="Due Date" />
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
