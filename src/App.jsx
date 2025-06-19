b83b23c15189b46b3f34f584e05c4b215ad0855a
Daniel <d.napieraj06@gmail.com>
2025-06-19 13:50:28 +0200

    Add task management functionality: implement task addition and deletion; create NewTask and Tasks components for task handling.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,25 @@ function App() {
 	const [projectsState, setProjectState] = useState({
 		selectedProjectId: undefined,
 		projects: [],
+		tasks: [],
 	})
+
+	const handleAddTask = text => {
+		setProjectState(prevState => {
+			const taskId = Math.random()
+			const newTask = {
+				text: text,
+				projectId: prevState.selectedProjectId,
+				id: taskId,
+			}
+			return {
+				...prevState,
+				tasks: [newTask, ...prevState.tasks],
+			}
+		})
+	}
+
+	const handleDeleteTask = () => {}
 
 	const handleSelectProject = id => {
 		setProjectState(prevState => {
@@ -51,15 +69,37 @@ function App() {
 			}
 		})
 	}
+
+	const handleDeleteProject = () => {
+		setProjectState(prevState => {
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+				projects: prevState.projects.filter(project => {
+					project.id !== prevState.selectedProjectId
+				}),
+			}
+		})
+	}
+
 	const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-	let content = <SelectedProject project={selectedProject} />
+	let content = (
+		<SelectedProject
+			project={selectedProject}
+			onDelete={handleDeleteProject}
+			onAddTask={handleAddTask}
+			onDeleteTask={handleDeleteTask}
+			tasks={projectsState.tasks}
+		/>
+	)
 
 	if (projectsState.selectedProjectId === null) {
 		content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
 	} else if (projectsState.selectedProjectId === undefined) {
 		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
 	}
+
 	return (
 		<main className="h-screen  flex gap-8 ">
 			<ProjectsSidebar
